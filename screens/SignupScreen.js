05404f8b18174674a885c2840d67f341b3797522
Lungu Mihai Adrian <lungu.snk199@gmail.com>
2022-11-08 09:16:32 +0200

    add project to github

---
+++ screens/SignupScreen.js
@@ -0,0 +1,31 @@
+import { useContext, useState } from 'react';
+import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { AuthContext } from '../store/auth-context';
+import { createUser } from '../util/auth';
+
+function SignupScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const authCtx = useContext(AuthContext);
+
+  async function signupHandler({email, password}) {
+    setIsAuthenticating(true);
+
+    try{
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
+    } catch(error) {
+      Alert.alert('Signup failed', 'Please check your credentials.');
+      setIsAuthenticating(false);
+    }
+  }
+
+  if(isAuthenticating) {
+    return <LoadingOverlay message='Creating user...' />
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
+}
+
+export default SignupScreen;
